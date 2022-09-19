f24216a6df19f45fb60d855676e6edb171a1ff28
Mick <mix2444@gmail.com>
2022-09-19 17:10:26 +1000

    x

--- vue/Themill/src/components/HomeMain.vue
+++ vue/Themill/src/components/HomeMain.vue
@@ -1,22 +1,33 @@
 <script setup>
-    import { ref, onBeforeMount, reactive } from "vue"
+    import { reactive } from "vue"
     
     const state = reactive({ 
-        selected: [],
-        groups: []
+        selected: [],   // Group selected
+        groups: [],     // List of groups
     })
 
     fetch('groups.json')
     .then(res => res.json())
-    .then(data => state.groups = data)
+    .then(data => { 
+        state.groups = data
+        if (state.groups.length) {
+            let currentID = state.groups[0].id
+            groupSelect(currentID)
+        }
+    })
 
-    let clicked = (event) => {
-        fetch(event.target.innerText + '.json')
+    const groupSelect = (groupID) => {
+        fetch('group' + groupID + '.json')
             .then(res => res.json())
             .then(data => {
                 state.selected = data
-            })
+        })
     }
+
+    const logSelect = (object) => {
+        console.log(object.id)
+    }
+
 </script>
 
 <template>
@@ -25,7 +36,7 @@
     <!-- buttons -->
     <ul class="nav flex-column text-center mx-auto group-buttons">
         <li v-for="group in state.groups" :key="group.id" class="nav-item">
-            <a @click="clicked" class="nav-link" href="#">{{ group.name }}</a>
+            <a @click="groupSelect(group.id)" class="nav-link" href="#">{{ group.name }}</a>
         </li>
     </ul>
 
@@ -34,7 +45,9 @@
         
         <div v-for="object in state.selected" :key="object.id" class="card list-group-item border-light shadow mb-3">
             <div class="card-body">
-                <a class="card-title fs-6"><RouterLink to="/LogView">{{ object.title }}</RouterLink></a>
+                <a @click="logSelect(object)" class="card-title">
+                <RouterLink to="/LogView/object.id">{{ object.title }}</RouterLink>
+                </a>
                 <p class="card-subtitle my-3">{{ object.date }}</p>
                 <p class="card-text">{{ object.description.substring(0, 60) }} .....</p>
             </div>
